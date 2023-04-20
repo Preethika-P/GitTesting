efa4e7f37d08a5fdf1380f5ac30cd3b2851b2930
AFTAB AHMED <aftabemir123@gmail.com>
2023-04-20 13:07:00 +0530

    BS

--- code/main.java
+++ code/main.java
@@ -6,5 +6,30 @@ public class main{
         int abc=123;
         
         System.out.println("Changing this line from my side");
+        binarySearch bs=new binarySearch();
+        bs.searchEle=1;
+        System.out.println("Finding serach ele b/w 0 and 10");
+        System.out.println(bs.binaryserachfn(0, 10));
+    }
+}
+
+
+class binarySearch{
+    public int searchEle=0;
+    public int binaryserachfn(int low,int high){
+        int mid=(low+high)/2;
+        if(searchEle==mid){
+            System.out.println("Found search ele"+searchEle);
+            return searchEle;
+        }
+        if(searchEle <mid)
+        {   
+            System.out.println("Calling with mid-1");
+            return binaryserachfn(low, mid-1);
+        }else{
+            System.out.println("calling with mid+1");
+            return binaryserachfn(mid+1, high);
+        }
+      
     }
 }
