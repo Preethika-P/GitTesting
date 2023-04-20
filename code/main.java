7a4360db899ed12fcedc1c32053e289693986b59
AFTAB AHMED <aftabemir123@gmail.com>
2023-04-20 13:12:48 +0530

    agaa

--- code/main.java
+++ code/main.java
@@ -24,7 +24,7 @@ class binarySearch{
         }
         if(searchEle <mid)
         {   
-            System.out.println("Calling with mid-1");
+            System.out.println("Calling with mid--1");
             return binaryserachfn(low, mid-1);
         }else{
             System.out.println("calling with mid+1");
